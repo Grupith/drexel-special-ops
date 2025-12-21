97e741d70d28eaf1e3121a07426cd50a5a2be3fb
Grupith <dylanhkoss@gmail.com>
2025-12-20 22:41:57 -0600

    fix: standardize string quotes in utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
 // Placeholder utility function for class names
 export function cn(...classes: string[]): string {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
