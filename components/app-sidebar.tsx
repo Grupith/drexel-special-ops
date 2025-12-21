3f44531bc37fa830f2440785add3c6043e2508c0
Grupith <dylanhkoss@gmail.com>
2025-12-20 23:14:32 -0600

    styled a basic dashboard page

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+import { GalleryVerticalEnd, Settings2, SquareTerminal } from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+// Sidebar data
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "P.O Splitter",
+      logo: GalleryVerticalEnd,
+      plan: "Drexel Special Ops",
+    },
+  ],
+  navMain: [
+    {
+      title: "New Split",
+      url: "/new-split",
+      icon: SquareTerminal,
+    },
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: Settings2,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
